9413db495aebc7be1bfaa7cf0b8493fcc25522b3
Bianca Pereira <bianca.oli.pereira@gmail.com>
2016-04-19 21:53:17 +0100

    Including extractors for subject and language

--- gutenberg/query/extractors.py
+++ gutenberg/query/extractors.py
@@ -8,6 +8,7 @@ from rdflib.term import URIRef
 
 from gutenberg._domain_model.vocabulary import DCTERMS
 from gutenberg._domain_model.vocabulary import PGTERMS
+from gutenberg._domain_model.vocabulary import RDFNAMESPACE
 from gutenberg._util.abc import abstractclassmethod
 from gutenberg.query.api import MetadataExtractor
 
@@ -36,7 +37,6 @@ class _SimplePredicateRelationshipExtractor(MetadataExtractor):
         query = cls._metadata()[:cls.predicate():cls.contains(requested_value)]
         return frozenset(cls._uri_to_etext(result) for result in query)
 
-
 class AuthorExtractor(_SimplePredicateRelationshipExtractor):
     """Extracts book authors.
 
@@ -52,7 +52,6 @@ class AuthorExtractor(_SimplePredicateRelationshipExtractor):
     @classmethod
     def contains(cls, value):
         return Literal(value)
-
 
 class TitleExtractor(_SimplePredicateRelationshipExtractor):
     """Extracts book titles.
@@ -86,3 +85,90 @@ class FormatURIExtractor(_SimplePredicateRelationshipExtractor):
     @classmethod
     def contains(cls, value):
         return URIRef(value)
+
+class _ComplexPredicateRelationshipExtractor(MetadataExtractor):
+    """Extracts any sort of meta-data that connected through two-level
+    relationships.
+
+    """
+
+    @abstractclassmethod
+    def predicate(cls):
+        """Returns the predicate that connects the text with another
+        entity (e.g. subject, language).
+
+        """
+        raise NotImplementedError
+
+    @abstractclassmethod
+    def secondary_predicate(cls):
+        """Extracts any sort of meta-data that is directly connected to a text via
+        a simple predicate relationship or a simple predicate-path relationship.
+
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_metadata(cls, etextno):
+        etext = cls._etext_to_uri(etextno)
+        query = cls._metadata()[etext:cls.predicate():]
+
+        metadata = set()
+        for result in query:
+            query2 = cls._metadata()[result::]
+            metadata.update(set((s.toPython(),p.toPython()) for s,p in query2))
+
+        # TODO: Improve presentation
+        return frozenset(metadata)
+
+    @classmethod
+    def get_etexts(cls, requested_value):
+        query = cls._metadata()[:cls.secondary_predicate():cls.contains(requested_value)]
+
+        etext = set()
+        for result in query:
+            query2 = cls._metadata()[:cls.predicate():result]
+            etext.update(set(cls._uri_to_etext(result) for result in query2))
+
+        return frozenset(etext)
+
+
+class SubjectExtractor(_ComplexPredicateRelationshipExtractor):
+    """Extracts book subjects.
+
+    """
+    @classmethod
+    def feature_name(cls):
+        return 'subject'
+
+    @classmethod
+    def predicate(cls):
+        return DCTERMS.subject
+
+    @classmethod
+    def secondary_predicate(cls):
+        return RDFNAMESPACE.value
+
+    @classmethod
+    def contains(cls,value):
+        return Literal(value)
+
+class LanguageExtractor(_ComplexPredicateRelationshipExtractor):
+    """Extracts book language.
+
+    """
+    @classmethod
+    def feature_name(cls):
+        return 'language'
+
+    @classmethod
+    def predicate(cls):
+        return DCTERMS.language
+
+    @classmethod
+    def secondary_predicate(cls):
+        return RDFNAMESPACE.value
+
+    @classmethod
+    def contains(cls,value):
+        return Literal(value,datatype=DCTERMS.RFC4646)
